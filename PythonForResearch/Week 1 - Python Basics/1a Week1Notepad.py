f00855c1e3e4cabc5e7ddf9580f73cd10029185e
Herman Woo - Windows <hermanwoo100@gmail.com>
2024-05-03 18:22:17 -0400

    Updating Lesson 1 for Python

--- PythonForResearch/Week 1 - Python Basics/1a Week1Notepad.py
+++ PythonForResearch/Week 1 - Python Basics/1a Week1Notepad.py
@@ -5,8 +5,14 @@ import numpy as num
 x = 2
 y = 3
 
-# List declaration
+# Mutable Object
 arr1 = [1,2,3,4,5]
+dict = {'key1': 'value1',
+        'key2': 2}
+print(dict.get("key2"))
+dict['key3'] = True
+print(dict.get("key3"))
+print("array 1: ", arr1, " Map 1: ",dict)
 
 # Immutable objects
 name = "Herman"
@@ -16,9 +22,35 @@ dob = (1993,2,25)
 numTuple = (1,2,3,4)
 sumOfTuple = sum(numTuple)
 print("Tuple: ", numTuple, " Sums up to: ",sumOfTuple)
-print(numTuple[0])
 
 
 print("Week 1 Overview:",
   num.sqrt(25)
 )
+
+
+# Dynamic Typing
+'''
+This is something new to me so i want to note this down,
+L1 = [1,2,3]
+L2 = L1
+
+L2 is not a new copy of L1. the array [1,2,3] is stored as an object
+L1 is stored as a variable name, and then there is a reference from L1 to the array
+Since variable names cannot be reference, L2 does not reference L1 but instead the array that L1 references
+
+L1[0] = 21
+This sets the array to [21,2,3]
+
+So therefore, L2[0] now also = 21 because L2 points to the same thing L1
+'''
+
+L1 = [1,2,3]
+L2 = L1
+L1[0] = 21
+''' and therefore if you use the 'is' keyword L2 and L1 would return True'''
+print(L2[0], L2 is L1)
+
+M = [10,20,30]
+N = [10,20,30]
+print(M == N, M is N)
--- PythonForResearch/Week 1 - Python Basics/NoteOnObjects.py
+++ PythonForResearch/Week 1 - Python Basics/NoteOnObjects.py
@@ -1,11 +1,5 @@
-x = [1,2,3,4,5,6]
-y = [11,12,14,15,17]
-
-##Function definition
-def printer(a):
-    for item in a:
-        print(a)
-
-
-printer(x)
-printer(y)
+import copy
+x = [1,[2]]
+y = copy.copy(x)
+z = copy.deepcopy(x)
+y is z
