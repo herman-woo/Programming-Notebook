a9a19e92bcbe9bf41fd7f626a15ae7a4c1ca808d
Hermanm <hermanwoo100@gmail.com>
2024-05-02 22:24:47 -0400

    update

---
+++ PythonForResearch/Week 1 - Python Basics/1a Week1Notepad.py
@@ -0,0 +1,24 @@
+## Week 1 overview
+import numpy as num
+
+# Basic Variable declarations
+x = 2
+y = 3
+
+# List declaration
+arr1 = [1,2,3,4,5]
+
+# Immutable objects
+name = "Herman"
+dob = (1993,2,25)
+
+
+numTuple = (1,2,3,4)
+sumOfTuple = sum(numTuple)
+print("Tuple: ", numTuple, " Sums up to: ",sumOfTuple)
+print(numTuple[0])
+
+
+print("Week 1 Overview:",
+  num.sqrt(25)
+)
--- PythonForResearch/Week 1 - Python Basics/Week1Notepad.py
+++
@@ -1,8 +0,0 @@
-##Week 1 overview
-import numpy as num
-
-x = 2
-
-print("Week 1 Overview:",
-      num.sqrt(25)
-    )
